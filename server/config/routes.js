5a081389d0957a262ecd6412eac372da87af3be4
Jasper Shau <jasper.shau@gmail.com>
2014-11-26 12:29:29 -0800

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -30,6 +30,7 @@ exports.myTeams = function(req, res) {
         .request(req, res)
         .api('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nba/leagues?format=json')
         .then(function(data) {
+           console.log(data);
             var leagues = data.fantasy_content.users[0].user[1].games[0].game[1].leagues;
             return leagues;
         })
